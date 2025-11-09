37bfe9caf98d8ac2741114f761ed7df5dc55c07c
rozangangsar <rozangangsar01@gmail.com>
2025-11-09 19:33:12 +0700

    update register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -12,7 +12,7 @@ export const ping = (req, res) => {
   res.json({ ok: true, time: new Date().toISOString() });
 };
 
-/*export const register = async (req, res, next) => {
+export const register = async (req, res, next) => {
   try {
     const { name, email, password, role = "parent" } = req.body;
     if (!email || !password) return res.status(400).json({ msg: "email & password required" });
@@ -26,7 +26,7 @@ export const ping = (req, res) => {
 
     res.status(201).json({ id: user._id, name: user.name, email: user.email, role: user.role });
   } catch (err) { next(err); }
-}; */
+};
 
 
 export const login = async (req, res) => {
