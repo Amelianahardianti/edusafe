bf680f4d57afe269a93e6359cfd516bfc15b12e1
rozangangsar <rozangangsar01@gmail.com>
2025-11-17 01:30:58 +0700

    logika botton hapus

--- frontend/app/components/userPage/NotificationSaranPage.jsx
+++ frontend/app/components/userPage/NotificationSaranPage.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 
-const NotificationKelasPage = ({headline ="", text="" , button=""}) => {
+const NotificationSaranPage = ({headline ="", text="", button = "Hapus", onDelete }) => {
     return (
         <div className="w-[80vw] h-auto border p-[2vh] overflow-hidden rounded-lg">
             <h1 className="font-bold text-2xl py-[2vh] ">{headline}</h1>
@@ -12,7 +12,9 @@ const NotificationKelasPage = ({headline ="", text="" , button=""}) => {
 <div>
 
 </div>
-        <div className="flex justify-end "><motion.button className=" text-white p-[1vh] rounded-lg"
+        <div className="flex justify-end ">
+            <motion.button className=" text-white p-[1vh] rounded-lg"
+            onClick={onDelete}
         initial={{ scale: 1, backgroundColor:"#0B3869"}}
         whileHover={{ scale: 1.1, backgroundColor:"#1e40af"}}>{button}</motion.button></div>
 
@@ -20,4 +22,4 @@ const NotificationKelasPage = ({headline ="", text="" , button=""}) => {
     );
 }
 
-export default NotificationKelasPage;
+export default NotificationSaranPage;
