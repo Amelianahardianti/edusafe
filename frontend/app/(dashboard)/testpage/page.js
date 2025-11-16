a99bc9fff9c1caec245238c1ac8b2d7c99a37bce
amelianahardianti <amelianahardiantiutari@mail.ugm.ac.id>
2025-11-16 11:17:09 +0700

    update 403

--- frontend/app/(dashboard)/testpage/page.js
+++ frontend/app/(dashboard)/testpage/page.js
@@ -1,13 +1,5 @@
+import Forbidden from "@/app/components/userPage/forbidden";
 
-
-
-export default function TestPage() {
-    return (
-        <div className="p-6 h-screen ">
-            <h1 className="text-2xl font-bold mb-4">Halaman Test</h1>
-            <p>Ini adalah halaman test di dalam layout dashboard.</p>
-           
-        </div>
-    );
-
+export default function ForbiddenPage() {
+  return <Forbidden />;
 }
