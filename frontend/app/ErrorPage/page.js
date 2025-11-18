65e696e7ffbe51a478671e23f981b690107145de
amelianahardianti <amelianahardiantiutari@mail.ugm.ac.id>
2025-11-18 22:08:30 +0700

    FORBIDEN

--- frontend/app/ErrorPage/page.js
+++ frontend/app/ErrorPage/page.js
@@ -3,7 +3,7 @@
 import Navbar from "@/app/components/userPage/NavbarDesktop";
 import Image from "next/image";
 import Link from "next/link";
-import Forbidden from "@/app/components/userPage/Forbidden";
+import Forbidden from "@/app/components/userPage/forbidden";
 
 export default function NotFoundPage(){
     return(
